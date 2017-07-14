9a253845910ae1fe00328a4586129124044a1ace
ligi <ligi@ligi.de>
2017-07-14 02:29:02 +0200

    Ignore connection exceptions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,24 +1,29 @@
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import java.net.ConnectException
 
 fun main(args: Array<String>) {
 
-    var last="ON"
+    var last = "ON"
 
     val build = OkHttpClient.Builder().build()
     while (true) {
         Thread.sleep(1000)
-        val result = build.newCall(Request.Builder().url("http://127.0.0.1:9999").build()).execute().body().string()
+        try {
+            val result = build.newCall(Request.Builder().url("http://127.0.0.1:9999").build()).execute().body().string()
 
-        if (result!=last) {
-            println(result)
-            last = result
-            val cmd=when(result) {
-                "ON" -> "xset dpms force on"
-                "OFF" -> "xset dpms force off"
-                else -> ""
+            if (result != last) {
+                println(result)
+                last = result
+                val cmd = when (result) {
+                    "ON" -> "xset dpms force on"
+                    "OFF" -> "xset dpms force off"
+                    else -> ""
+                }
+                Runtime.getRuntime().exec(cmd)
             }
-            Runtime.getRuntime().exec(cmd)
+        } catch (e: ConnectException) {
+            println("ignored error: " + e.message)
         }
     }
 }
